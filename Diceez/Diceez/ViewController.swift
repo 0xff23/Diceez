5ae79e654f4905bbea95fabeb62b331be3f41ff8
Kirill <kgnix0@gmail.com>
2018-01-08 00:56:48 -0500

    UI constrains left

--- Diceez/Diceez/ViewController.swift
+++ Diceez/Diceez/ViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var diceOne: UIImageView!
+    @IBOutlet var diceTwo: UIImageView!
+    
+    var diceRollIndexOne: Int = 0
+    var diceRollIndexTwo: Int = 0
+    
+    let diceImages = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateDiceImage()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +28,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func rollDice(_ sender: Any) {
+        updateDiceImage()
+    }
+    
+    func updateDiceImage() {
+        diceRollIndexOne = Int(arc4random_uniform(6))
+        diceRollIndexTwo = Int(arc4random_uniform(6))
+        
+        diceOne.image =  UIImage(named: diceImages[diceRollIndexOne])
+        diceTwo.image =  UIImage(named: diceImages[diceRollIndexTwo])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImage()
+    }
 }
 
